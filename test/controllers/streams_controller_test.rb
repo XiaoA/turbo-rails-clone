b84a992085f6325b410951b72f6856554cae2afd
Andrew Buckingham <abuckingham99@gmail.com>
2022-07-14 22:07:46 -0700

    Create custom test assertion and integration tests for `engine.rb`

--- test/controllers/streams_controller_test.rb
+++ test/controllers/streams_controller_test.rb
@@ -4,6 +4,9 @@ class TurboClone::StreamsControllerTest < ActionDispatch::IntegrationTest
   test "create with respond_to?" do
     post articles_path, params: { article: { content: "content" } }
     assert_redirected_to articles_path
+
+    post articles_path, params: { article: { content: "content" } }, as: :turbo_stream
+    assert_turbo_stream action: "prepend", target: "articles"
   end
 
   test "update with respond_to?" do
@@ -11,6 +14,9 @@ class TurboClone::StreamsControllerTest < ActionDispatch::IntegrationTest
 
     patch article_path(article), params: { article: { content: "update" } }
     assert_redirected_to articles_path
+
+    patch article_path, params: { article: { content: "update" } }, as: :turbo_stream
+    assert_turbo_stream action: "replace", target: article
   end
 
   test "destroy with respond_to?" do
@@ -18,5 +24,10 @@ class TurboClone::StreamsControllerTest < ActionDispatch::IntegrationTest
 
     delete article_path(article)
     assert_redirected_to articles_path
+
+    article = Article.create! content: "content"
+
+    delete article_path(article), as: :turbo_stream
+    assert_turbo_stream action: "remove", target: article
   end
 end
