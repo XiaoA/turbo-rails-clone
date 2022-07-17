fbed16da6a7b4a0fcf5b601aa18485c3329b9de2
Andrew Buckingham <abuckingham99@gmail.com>
2022-07-17 14:37:30 -0700

    Test "#broadcast_replace_to" method

--- test/channels/broadcastable_test.rb
+++ test/channels/broadcastable_test.rb
@@ -18,4 +18,12 @@ class TurboClone::BroadcastableTest < ActionCable::Channel::TestCase
       article.broadcast_prepend_to "stream", target: "prepended article"
     end
   end
+
+  test "#broadcast_replace_to" do
+    article = Article.create! content: "content"
+
+    assert_broadcast_on "stream", turbo_stream_action_tag(:replace, target: article, template: render(article)) do
+      article.broadcast_replace_to "stream"
+    end
+  end  
 end
