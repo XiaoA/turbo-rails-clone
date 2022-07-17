1e71321737da1e4c8e881055d8baef065d6fe699
Andrew Buckingham <abuckingham99@gmail.com>
2022-07-17 14:34:39 -0700

    Test "#broadcast_prepend_to" method

--- test/channels/broadcastable_test.rb
+++ test/channels/broadcastable_test.rb
@@ -10,4 +10,12 @@ class TurboClone::BroadcastableTest < ActionCable::Channel::TestCase
       article.broadcast_append_to "stream"      
     end
   end
+
+  test "#broadcast_prepend_to" do
+    article = Article.create! content: "content"
+
+    assert_broadcast_on "stream", turbo_stream_action_tag(:prepend, target: "prepended article", template: render(article)) do
+      article.broadcast_prepend_to "stream", target: "prepended article"
+    end
+  end
 end
